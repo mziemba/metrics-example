70c01c8d088d799aed5c7fec1ac4eea8536d8e46
Michał Ziemba <ziembal86@gmail.com>
2014-02-27 22:24:21 +0100

    Fix resources folder

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,7 +12,11 @@ CONFIG_PATH=src/main/resources
 CLASSPATH=$JAR_FILE
 
 # Enable JMX for reporting
-VM_ARGS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=51111 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+VM_ARGS=""\
+"-Dcom.sun.management.jmxremote "\
+"-Dcom.sun.management.jmxremote.port=51111 "\
+"-Dcom.sun.management.jmxremote.authenticate=false "\
+"-Dcom.sun.management.jmxremote.ssl=false"
 
 mvn clean assembly:assembly -DskipTests
 java $* $VM_ARGS -cp $CLASSPATH $CLASS_NAME
